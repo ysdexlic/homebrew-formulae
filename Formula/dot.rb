0e56de0df24679e0c6e6cfa0a6f245e48b5b75ec
David Thompson <david.arthur.thompson@gmail.com>
2021-03-10 09:50:40 +0000

    dot 0.1.2
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/dot.rb
+++ Formula/dot.rb
@@ -1,8 +1,8 @@
 class Dot < Formula
   desc "Dotfile Manager"
   homepage "https://github.com/ysdexlic/dot"
-  url "https://github.com/ysdexlic/dot/archive/v0.1.0.tar.gz"
-  sha256 "ff5aafcaa10ef0a46b38f294d3569376072f011b4abfe64c30a15cc5626de489"
+  url "https://github.com/ysdexlic/dot/archive/v0.1.2.tar.gz"
+  sha256 "75fc908870d9d7802819580449f0b8d814ccadab0c1a37e5cd8dae11b342cc8f"
   license "MIT"
   head "https://github.com/ysdexlic/dot.git"
 
